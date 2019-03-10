d2bcc550d06cd76366da276329c30e2545f4f40e
josepablito16 <josepablocif16@gmail.com>
2019-03-09 22:12:05 -0600

    Conexion inicial de firebase con python3

---
+++ Python/a.py
@@ -0,0 +1,26 @@
+
+
+def getData(root,child=None):
+	from firebase import firebase
+	var='/'
+	var+=root
+	firebase = firebase.FirebaseApplication('https://ssaporfam-e5e70.firebaseio.com')
+	result = firebase.get(var, child)
+	return result
+
+def updateOrSet(root,child,value):
+	from firebase import firebase
+	firebase = firebase.FirebaseApplication('https://ssaporfam-e5e70.firebaseio.com')
+	result = firebase.put(root, child,value)
+
+def delete(root,child=None):
+	from firebase import firebase
+	var='/'
+	var+=root
+	firebase = firebase.FirebaseApplication('https://ssaporfam-e5e70.firebaseio.com')
+	result = firebase.delete(var, child)
+
+##MAIN
+print (getData('user','two'))
+#updateOrSet('user','jose',10)
+#delete('user/jose')
---
+++ Python/readMe.txt
@@ -0,0 +1,9 @@
+Pasos a seguir
+1. copiar la carpeta firebase a Python37-32\Lib
+
+2. Dentro de la carpeta Lib de tu Python, debes cambiar los archivos [async.py] a [asyncn]
+
+3. ejecutar el siguiente comando [pip install requests --upgrade] ya que necesitamos que este en la ultima version 
+
+
+El archivo a correr es [a.py] en python3
